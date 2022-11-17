b246e9e143efd3d46bded0aa87a7e738e1f77645
j3thr0Cruz <j3thro.cruz@gmail.com>
2022-11-16 20:42:44 -0500

    brocode lessons

---
+++ lessons.py
@@ -0,0 +1,14 @@
+#String Methods
+
+name = "bro code"
+
+#print(len(name))
+#print(name.find("B")) #find index location
+print(name.capitalize())
+print(name.upper())
+print(name.lower())
+print(name.isdigit())
+print(name.isalpha())
+print(name.count("o"))
+print(name.replace("o","a"))
+print(name*3)
